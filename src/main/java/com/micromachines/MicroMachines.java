576175e0f37a7892de7ccf9ddbf64c3bcf209afb
UtopiaXC <49057287+UtopiaXC@users.noreply.github.com>
2019-07-16 18:29:11 +0800

    First push
    First push

---
+++ src/main/java/com/micromachines/MicroMachines.java
@@ -0,0 +1,29 @@
+package com.micromachines;
+
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = MicroMachines.MODID, name = MicroMachines.NAME, version = MicroMachines.VERSION)
+public class MicroMachines {
+    public static final String MODID = "micromachines";
+    public static final String NAME = "Micro Machines";
+    public static final String VERSION = "1.0.0";
+
+    private static Logger logger;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
